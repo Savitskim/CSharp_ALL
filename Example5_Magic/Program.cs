f0ebd32aec00756ee3b26381800713acd83e866f
Savitskim <savitskim@inbox.ru>
2022-11-09 21:09:42 +0300

    End Lesson

--- Example5_Magic/Program.cs
+++ Example5_Magic/Program.cs
@@ -5,11 +5,11 @@ int xa = 40, ya = 1,
     xb = 1, yb = 30, 
     xc = 80, yc = 30;
 Console.SetCursorPosition(xa,ya);
-Console.WriteLine("+");
+Console.WriteLine("#");
 Console.SetCursorPosition(xb,yb);
-Console.WriteLine("+");
+Console.WriteLine("#");
 Console.SetCursorPosition(xc,yc);
-Console.WriteLine("+");
+Console.WriteLine("#");
 
 int x = xa, y = ya;
 
@@ -33,6 +33,6 @@ while(count<10000)
         y = (y+yc)/2;
     }
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
+    Console.WriteLine("#");
     count++;
 }
