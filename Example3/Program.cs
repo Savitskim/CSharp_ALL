6300ef886c0e56e2bea7aeaf378faf3ea331513e
Savitskim <savitskim@inbox.ru>
2022-11-09 20:29:05 +0300

    Привет Маша

---
+++ Example3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя:");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Я так двано тебя ждал Маша");
+}
+else
+{
+    Console.Write("Ну привет ");
+    Console.Write(username);
+}
