c153060799265bb9463e76fe9f777be05c5ee30d
Savitskim <savitskim@inbox.ru>
2022-11-15 18:39:18 +0300

    Seminar2

---
+++ Example7/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 12, 43, 12, 45, 2, 4, 7, 12 };
+Console.WriteLine("Введите число");
+int find = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int size = array.Length;
+while (index < size)
+{
+    if (array[index] == find) Console.WriteLine($"Значение элемента массива с индексом - {index} , соответствует числу {find}");
+    index++;
+}
+// break; - прервать
